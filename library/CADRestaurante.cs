9864871a3f20272560002e6fa77b80101dc04db0
nickhernm <ramajahede@gmail.com>
2024-04-18 10:03:26 +0200

    48776654W: Creados y realizados EN/CAD de Cliente-Restaurante-Direccion

--- library/CADRestaurante.cs
+++ library/CADRestaurante.cs
@@ -1,8 +1,59 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Configuration;
 
-public class CADRestaurante
+namespace library 
 {
-	public CADRestaurante()
+	public class CADRestaurante
 	{
+    	private string con;
+
+    	public CADRestaurante()
+    	{
+    	    con = ConfigurationManager.ConnectionStrings["Database"].ToString();
+    	}
+
+    	public bool Create(ENRestaurante en)
+    	{
+    	    // Implementación del método Create
+    	}
+
+    	public bool Read(ENRestaurante en)
+    	{
+    	    // Implementación del método Read
+    	}
+
+    	public bool readFirst(ENRestaurante en)
+    	{
+    	    // Implementación del método readFirst
+    	}
+
+    	public bool readNext(ENRestaurante en)
+    	{
+    	    // Implementación del método readNext
+    	}
+
+    	public bool readPrev(ENRestaurante en)
+    	{
+    	    // Implementación del método readPrev
+    	}
+
+    	public bool Update(ENRestaurante en)
+    	{
+    	    // Implementación del método Update
+    	}
+
+    	public bool Delete(ENRestaurante en)
+    	{
+    	    // Implementación del método Delete
+    	}
 	}
+
 }
