70ee4071e6c798fc1da4c59cb5dc0082febfbda1
DarioSF <dario.sim.fra@gmail.com>
2024-04-19 12:03:26 +0200

    74392984J: EN CAD creados

---
+++ library/CADPedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace library
+{
+	public class CADPedido
+	{
+		public CADPedido()
+		{
+			public bool Create(ENPedido en)
+			{
+
+			}
+
+			public bool Delete(ENPedido en)
+			{
+
+			}
+
+			public bool Update(ENPedido en)
+			{
+
+			}
+
+			public bool Rate(ENPedido en)
+			{
+
+			}
+		}
+	}
+}
+
---
+++ library/ENPedido.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace library
+{
+    public class ENPedido
+    {
+        private int numPedido;
+
+        /*
+            Hablar con el grupo para ver como mejorar el 
+            pedido/linea de pedido
+         */
+
+        public int NumPedido
+        {
+            get { return numPedido; }
+            set { numPedido = value; }
+        }
+
+        public ENPedido()
+        {
+            this.numPedido = null;
+        }
+
+        public ENPedido(int num)
+        {
+            this.numPedido = num;
+        }
+
+        public bool Create()
+        {
+
+        }
+
+        public bool Update()
+        {
+
+        }
+
+        public bool Delete()
+        {
+
+        }
+
+        public bool Rate()
+        {
+
+        }
+    }
+}
+
+
+
+
