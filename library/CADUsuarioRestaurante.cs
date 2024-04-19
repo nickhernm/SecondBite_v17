b73037dc94ad1bc0ec50e9a637e911d687bfd591
DarioSF <dario.sim.fra@gmail.com>
2024-04-19 18:25:42 +0200

    74392984J: Añado EN CAD para usuario tipo restaurante

---
+++ library/CADUsuarioRestaurante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace library
+{
+	public class CADUsuarioRestaurante
+	{
+		public CADUsuarioRestaurante()
+		{
+			//crear base de datos y para poder conectar
+		}
+
+		public bool Create(ENUsuarioRestaurante en)
+		{
+			//TODO
+		}
+
+		public bool Delete(ENUsuarioRestaurante en)
+		{
+			//TODO
+		}
+
+		public bool Update(ENUsuarioRestaurante en)
+		{
+			//TODO
+		}
+
+		public bool Read(ENUsuarioRestaurante en)
+		{
+			//TODO
+		}
+	}
+}
