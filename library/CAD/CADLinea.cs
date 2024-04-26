c0146dd820e99431ee1a3667031daf92ea6824de
nb97-ua <123986673+nb97-ua@users.noreply.github.com>
2024-04-26 18:11:29 +0200

    actualizado

---
+++ library/CAD/CADLinea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace library
+{
+	public class CADLinea
+	{
+		public CADLinea()
+		{
+			//crear base de datos y para poder conectar
+		}
+
+		public bool Create(ENLinea en)
+		{
+			//TODO
+		}
+
+		public bool Delete(ENLinea en)
+		{
+			//TODO
+		}
+
+		public bool Update(ENLinea en)
+		{
+			//TODO
+		}
+
+		public bool Read(ENLinea en)
+		{
+			//TODO
+		}
+	}
+}
+
---
+++ library/EN/ENLinea.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace library
+{
+	public class ENLinea
+	{
+        public int Linea
+        {
+            get { return Linea; }
+            set { Linea = value; }
+        }
+
+        public int Importe
+        {
+            get { return Importe; }
+            set { Importe = value; }
+        }
+
+        public int Cantidad
+        {
+            get { return Cantidad; }
+            set { Cantidad = value; }
+        }
+
+		public ENLinea()
+		{
+            this.Linea = 0;
+            this.Importe = 0;
+            this.Cantidad = 0;
+		}
+
+        public ENLinea(int linea, int importe, int cantidad)
+        {
+            this.Linea = linea;
+            this.Importe = importe;
+            this.Cantidad = cantidad;
+        }
+
+        public bool Create()
+        {
+            CADLinea lin = new CADLinea();
+            bool create = lin.Create(this);
+        }
+
+        public bool Update()
+        {
+            CADLinea lin = new CADLinea();
+            bool update = lin.Update(this);
+        }
+
+        public bool Delete()
+        {
+            CADLinea lin = new CADLinea();
+            bool delete = lin.Delete(this);
+        }
+
+        public bool Read()
+        {
+            CADLinea lin = new CADLinea();
+            bool read = lin.Read(this);
+        }
+    }
+}
