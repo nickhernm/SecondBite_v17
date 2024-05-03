392245b4b92cce2bfe05bccf39240d910247c806
DarioSF <dario.sim.fra@gmail.com>
2024-05-03 09:51:42 +0200

    Páginas principal y de perfil terminadas

--- Web/Perfil.aspx.designer.cs
+++ Web/Perfil.aspx.designer.cs
@@ -11,7 +11,16 @@ namespace Web
 {
 
 
-    public partial class _Default
+    public partial class Perfil
     {
+
+        /// <summary>
+        /// Control Login1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Login Login1;
     }
 }
