2a1b203b811ab50877e3edb03851a6c9b486ac2a
JAIME HERNANDEZ DELGADO <jhd3@mscloud.ua.es>
2024-05-10 15:13:35 +0200

    48776654W: Comienzo restaurante individual

--- Web/Menu_Individual.aspx.designer.cs
+++ Web/Menu_Individual.aspx.designer.cs
@@ -7,16 +7,20 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
+namespace Web
+{
 
 
-public partial class  Perfil{
+    public partial class WebForm1
+    {
 
-    /// <summary>
-    /// Control form1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
 }
