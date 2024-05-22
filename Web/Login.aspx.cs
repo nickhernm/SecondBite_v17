637eba83fadb5334aa0848c5b21ee5381a459c41
Yaroslav Horshynkyy Varlamov <95243632+YarosHor@users.noreply.github.com>
2024-05-22 12:46:13 +0200

    Update Login.aspx.cs

--- Web/Login.aspx.cs
+++ Web/Login.aspx.cs
@@ -1,6 +1,13 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using library;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace Web
 {
@@ -46,23 +53,6 @@ namespace Web
         {
             // Logic to retrieve dynamic content
             return "Dynamic card content";
-        }
-
-        private class ENUsuarioRestaurante
-        {
-            private string contrasena;
-            private string name;
-
-            public ENUsuarioRestaurante(string contrasena, string name)
-            {
-                this.contrasena = contrasena;
-                this.name = name;
-            }
-
-            internal bool CheckUser()
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 }
