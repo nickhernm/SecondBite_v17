da454144b3127cc5f4c9ffc791db32b06bd9977e
DarioSF <dario.sim.fra@gmail.com>
2024-05-22 13:43:01 +0200

    Pagina perfil con conexion a la bd

--- Web/Perfil.aspx.cs
+++ Web/Perfil.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,47 @@ namespace Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarDatosPerfil();
+            }
+        }
 
+        private void CargarDatosPerfil()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString;
+            string query = "SELECT NombreUsuario, Correo, FechaNacimiento, Direccion, Ciudad, CodigoPostal FROM Usuarios WHERE UsuarioID = @UsuarioID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@UsuarioID", 1); // Reemplaza con el ID del usuario actual
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            lblNombreUsuario.Text = reader["NombreUsuario"].ToString();
+                            lblCorreo.Text = reader["Correo"].ToString();
+                            lblFechaNacimiento.Text = Convert.ToDateTime(reader["FechaNacimiento"]).ToString("dd/MM/yyyy");
+                            lblDireccion.Text = reader["Direccion"].ToString();
+                            lblCiudad.Text = reader["Ciudad"].ToString();
+                            lblCodigoPostal.Text = reader["CodigoPostal"].ToString();
+                        }
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Manejar excepción
+                    Response.Write(ex.Message);
+                }
+            }
         }
     }
 }
--- Web/Perfil.aspx.designer.cs
+++ Web/Perfil.aspx.designer.cs
@@ -7,16 +7,65 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
+namespace Web
+{
 
 
-public partial class  Perfil{
+    public partial class Perfil
+    {
 
-    /// <summary>
-    /// Control form1.
-    /// </summary>
-    /// <remarks>
-    /// Campo generado automáticamente.
-    /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        /// <summary>
+        /// Control lblNombreUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombreUsuario;
+
+        /// <summary>
+        /// Control lblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCorreo;
+
+        /// <summary>
+        /// Control lblFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaNacimiento;
+
+        /// <summary>
+        /// Control lblDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDireccion;
+
+        /// <summary>
+        /// Control lblCiudad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCiudad;
+
+        /// <summary>
+        /// Control lblCodigoPostal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCodigoPostal;
+    }
 }
